fa0bdc4ea802e0c6cafdfb156fad21cb603674f3
jeffrettad <Adedokunadesoye3040@gmail.com>
2025-06-05 12:12:19 -0700

    .

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import PostCard from "@/components/common/PostCard";
 import { PostProps } from "@/interfaces";
 import Header from "@/components/layout/Header";
+["getStaticProps", "async", "await"]
 
 const PostsPage = () => {
   const [posts, setPosts] = useState<PostProps[]>([]);

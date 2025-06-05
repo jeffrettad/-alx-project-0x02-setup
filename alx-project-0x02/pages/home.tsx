8c8f266e74f9599667f524aa11831669ac73a3f0
jeffrettad <Adedokunadesoye3040@gmail.com>
2025-06-05 09:45:09 -0700

    add home.tsx

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,10 +1,15 @@
+import Card from "@/components/common/Card";
 const Home = () => {
-    return (
-      <main className="p-4">
-        <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
-      </main>
-    );
-  };
-  
-  export default Home;
-  
+  return (
+    <main className="p-6">
+      <h1 className="text-3x1 font-bold mb-4">Home Page</h1>
+      <div className="ggrid gap-4">
+        <Card title="welcome" content=" This is a reusable card component. "/>
+        <Card title="About the App" content="Tthis app is built with Next.js and typescript."/>
+        <Card title="Get Started" content="Expore the site using the navigation link above."/>
+      </div>
+    </main>
+  );
+};
+
+export default Home;

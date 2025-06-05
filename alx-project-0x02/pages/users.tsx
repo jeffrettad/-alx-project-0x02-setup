7c272a738340907508b1eb925278939b38ef93c0
jeffrettad <Adedokunadesoye3040@gmail.com>
2025-06-05 12:20:35 -0700

    .

---
+++ alx-project-0x02/pages/users.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
+import Header from "@/components/layout/Header";
+
+const UsersPage = () => {
+  const [users, setUsers] = useState<UserProps[]>([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <main className="p-6">
+        <h1 className="text-3xl font-bold mb-4">Users</h1>
+        {users.map((user) => (
+          <UserCard key={user.id} {...user} />
+        ))}
+      </main>
+    </>
+  );
+};
+
+export default UsersPage;

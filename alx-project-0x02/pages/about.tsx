8a87e6fc78717048ed2e3b930731a89725e57489
jeffrettad <Adedokunadesoye3040@gmail.com>
2025-06-05 08:52:25 -0700

    add home.tsx about.tsx

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,7 @@
+const About = () => {
+    return (
+        <main className="p-4">
+            <h1 className="text-2xl font-bold">About Us</h1>
+        </main>
+    );
+};
---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <main className="p-4">
+        <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
+      </main>
+    );
+  };
+  
+  export default Home;
+  
